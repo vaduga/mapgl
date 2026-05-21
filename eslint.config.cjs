ef5258fc4b8a054f16b706b3201385fa6f7fbbfe
vaduga <arbitr38@gmail.com>
2026-05-21 23:11:59 +0800

    fix: deprecated @stylistic/eslint-plugin-ts

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,4 +1,38 @@
-const grafanaConfig = require('@grafana/eslint-config/flat');
+const jsdoc = require('eslint-plugin-jsdoc');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const reactPlugin = require('eslint-plugin-react');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettierConfig = require('eslint-config-prettier');
+const stylistic = require('@stylistic/eslint-plugin');
+
+const grafanaBaseConfig = require('@grafana/eslint-config/base');
+
+const grafanaConfig = [
+  reactHooksPlugin.configs.flat['recommended-latest'],
+  reactPlugin.configs.flat.recommended,
+  prettierConfig,
+  {
+    name: '@grafana/eslint-config/flat',
+    settings: grafanaBaseConfig.settings,
+    plugins: {
+      jsdoc,
+      '@typescript-eslint': tsPlugin,
+      '@stylistic/ts': stylistic,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: grafanaBaseConfig.ecmaVersion,
+      sourceType: grafanaBaseConfig.sourceType,
+      parserOptions: grafanaBaseConfig.parserOptions,
+    },
+    rules: {
+      ...grafanaBaseConfig.rules,
+      '@stylistic/ts/type-annotation-spacing': ['error', { after: true, before: false }],
+      '@stylistic/ts/arrow-spacing': ['error', { after: true, before: true }],
+    },
+  },
+];
 
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
